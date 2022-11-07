6ef5a3611b456a0316f60c0efc507a07aec18508
DeveloperGentle <110312067+DeveloperGentle@users.noreply.github.com>
2022-11-07 02:48:26 +0100

    added favicon

---
+++ index.php
@@ -0,0 +1,144 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>e-Track</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <!-- <script src="https://kit.fontawesome.com/2026f77c4a.js" crossorigin="anonymous"></script> -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+   
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,400;1,700;1,800&display=swap" rel="stylesheet">
+    
+<link rel="stylesheet" href="home.css">
+   
+</head>
+<body>
+ <div id="root">
+    <nav>
+      <a href="#" class="headerLink"><img class="logo-ico" src="favicon.ico" alt="e"/><h1 class="logo">Track</h1></a>
+       <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Features</li>
+        <li>Contact</li>
+        <li><a href="signIn.php"><button class="sign-in-btn">Sign in</button></a></li>
+        <span class="menuBar">&#9776;</span>
+       </ul>
+     
+    </nav>
+    <ul class="mobile-ul">
+        <li class="mobileLink">Home</li>
+        <li class="mobileLink">About</li>
+        <li class="mobileLink">Features</li>
+        <li class="mobileLink">Contact</li>
+        <li class="mobileLink"><a href="signIn.php"><button class="sign-in-btn">Sign in</button></a></li>
+       </ul>
+    <div class="container">
+        <section class="home">
+            <div class="home-flex">
+                <h1 class="first-h1">Manage Your personal <br> finances effectively.</h1>
+                <!-- <h3>Keep track of your money <span> by using a smart expense tracker</span></h3> -->
+                
+
+                <p class='btnPara'><a href="signUp.php"><button class="get-started-btn">Get Started</button></a></p>
+
+            </div>
+           
+        </section>
+
+
+
+        <section class="second">
+            <div class="content2 moveRight">
+                <h1>What we do</h1>
+                <p>e-Track is your very own online money manager where you<br/>
+                can track your income and spendings anytime, anywhere!<br/> Gain control over your spending habits with e-Track.</p>
+            </div>
+
+            <div class="phones moveLeft">
+                <img class = "sec-phone" src="phone mockup 2.webp" alt="">
+            </div>
+        </section>
+
+        <section class="second">
+            <div class="phones moveRight">
+                <img class = "sec-phone" src="phone mockup 2.webp" alt="">
+            </div>
+           
+            <div class="content2 moveLeft">
+                <h1>How it Works</h1>
+                
+                <p style="margin-bottom: 40px;">Simply create an account with e-Track now and start tracking<br/> your spendings or sign in if you already have one.</p>
+                 <p class='btnPara'><a href="signUp.php"><button class="get-started-btn small">Get Started</button></a></p>
+            </div>
+
+        </section>
+
+        <section class="second">
+         <div class="content2 moveRight">
+             <h1>Your Very own <br/>expense tracker</h1>
+             <p>Ever wished you had a way to track your finances so <br/>you could know when to spend and what to spend on?<br/> 
+             well that way is e-Track!, an app that let's you control<br/>  your spendings and never be caught unaware.</p>
+         </div>
+
+         <div class="phones moveLeft">
+             <img class = "sec-phone" src="phone mockup 2.webp" alt="">
+         </div>
+     </section>
+        <section class="first">
+         <div class="head">
+             <h1>Features</h1>
+             
+         </div>
+
+         <div class="content">
+             <div class="each one">
+                 
+                 <h2>Keep track <br> easily</h2>
+
+                 <p>You don't need a book or pen to track your spendings.
+                 e-Track is your personal online money manager.</p>
+             </div>
+             <div class="each two">
+                 
+                 <h2>Personalized <br>experience</h2>
+
+                 <p>Be your own boss and know where you stand.
+                  Keep track  of your finances at your own pace with e-Track.</p>
+             </div>
+             <div class="each three">
+                 
+                 <h2>Make Money <br> Wise Decisions</h2>
+
+                 <p>Make money wise decisions based on your spending habits.
+                 Know your spending limits with just a look!</p>
+             </div>
+
+         </div>
+     
+     </section>
+
+     <section class="contact-section">
+      <h1>Contact us</h1>
+      <div class="contact-icons-flex">
+       <a target="_blank" href="https://whatsapp.com"><img src="whatsapp.svg" alt="whatsapp" class="contact-img"></a>
+       <a target="_blank" href="https://github.com/oriakhiosamu1/expensetracker"><img src="github.svg" alt="github" class="contact-img"></a>
+      </div>
+     </section>
+     <div class="fixedButtonContainer"><button type="button" class="goTop"><img src="arrow-up.svg" alt="go top" class="arrow-top"/></button></div>
+     <footer>
+        <p class="footer-p">Copyright © 2022. All rights reserved.</p>
+    </footer>
+
+        
+    </div>
+    
+    
+  
+    </div>
+   
+     <script src="script.js"></script>
+</body>
+</html>
